e8d5e02b33241f7e6372a54a280237190914bca8
Jeremy Wu <jeremy24wu@gmail.com>
2019-09-27 15:18:02 +1000

    Add tests

--- Wox.Test/FuzzyMatcherTest.cs
+++ Wox.Test/FuzzyMatcherTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using NUnit.Framework;
 using Wox.Infrastructure;
@@ -38,6 +39,56 @@ namespace Wox.Test
             Assert.IsTrue(results[0].Title == "Inste");
             Assert.IsTrue(results[1].Title == "Install Package");
             Assert.IsTrue(results[2].Title == "file open in browser-test");
+        }
+
+        [TestCase("c", 50)]
+        [TestCase("ch", 50)]
+        [TestCase("chr", 50)]
+        [TestCase("chrom", 50)]
+        public void FuzzyMatchSensitivityTest(string searchTerm, int searchSensitivity)
+        {
+            var sources = new List<string>
+            {
+                "Chrome",
+                "Choose which programs you want Windows to use for activities like web browsing, editing photos, sending e-mail, and playing music.",
+                "Candy Crush Saga from King",
+                "Uninstall or change programs on your computer",
+                "Add, change, and manage fonts on your computer"
+            };
+
+            var results = new List<Result>();
+            foreach (var str in sources)
+            {
+                results.Add(new Result
+                {
+                    Title = str,
+                    Score = FuzzyMatcher.Create(searchTerm).Evaluate(str).Score
+                });
+            }
+
+            results = results.Where(x => x.Score > searchSensitivity).OrderByDescending(x => x.Score).ToList();
+
+            Debug.WriteLine("");
+            Debug.WriteLine("###############################################");
+
+            Debug.WriteLine("SEARCHTERM: " + searchTerm + ", GreaterThanSearchSensitivityScore: " + searchSensitivity);
+            foreach (var item in results)
+                Debug.WriteLine("SCORE: " + item.Score.ToString() + ", SearchString: " + item.Title);
+
+            Debug.WriteLine("###############################################");
+            Debug.WriteLine("");
+
+
+
+            //Assert.IsTrue(results.Count == 5);
+
+
+
+            //Assert.IsTrue(results[0].Title == sources[1]);
+
+            //Assert.IsTrue(results[0].Title == sources[0]);
+            //Assert.IsFalse(results[1].Title == sources[1]);
+            //Assert.IsTrue(results.Count == 3);
         }
     }
 }
